bd4f6f073fba676f67b826f38a0b572b5efbd578
Rattasat160947 <rattasat0624987765@gmail.com>
2025-11-09 22:03:21 +0700

    edit config

--- backend/entity/Gender.go
+++ backend/entity/Gender.go
@@ -1,5 +1,4 @@
 package entity
-<<<<<<< HEAD
 
 import "gorm.io/gorm"
 
@@ -7,19 +6,3 @@ type Gender struct{
 	gorm.Model
 	Name string `json:"name"`
 }
-=======
-import "gorm.io/gorm"
-type Gender string
-
-const (
-	Male   Gender = "Male"
-	Female Gender = "Female"
-
-)
-
-type GenderInfo struct {
-	gorm.Model
-	Type Gender `json:"type"` 
-	// `gorm:"column:Gender;type:enum('Male','Female')"`
-}
->>>>>>> 98902e3764a6bd8322fcd39716a5dc3ec326169e
