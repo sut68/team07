2346da022b90fa712ef587482f6f2c46eebe6d2d
Rattasat160947 <rattasat0624987765@gmail.com>
2025-11-14 18:55:54 +0700

    Delete Rattasat Entity again

--- backend/entity/appointmentType.go
+++
@@ -1,9 +0,0 @@
-package entity
-import "gorm.io/gorm"
-
-type AppointmentType struct {
-	gorm.Model
-	Name string `json:"name"`
-
-	Appointments []Appointment `gorm:"foreignKey:AppointmentTypeID" json:"appointments"`
-}
