99ddca99099a009675f31dd5a6aee88bfb033b12
pthanathon <pthanathonhodon@gmail.com>
2025-11-15 19:52:22 +0700

    UI login

--- frontend/app/(modules)/login/page.tsx
+++ frontend/app/(modules)/login/page.tsx
@@ -1,29 +1,62 @@
-"use client"; 
+"use client";
 
 import { useRouter } from 'next/navigation';
+import "../../style/login.css"
+import loginbg from "../../../public/image/login-bg2.jpg"
 
 export default function LoginPage() {
   const router = useRouter();
 
   const handleLogin = (e: React.FormEvent) => {
     e.preventDefault();
-    router.push('/dashboard'); 
+    router.push('/dashboard');
   };
 
+  const overlayColor = 'rgba(0, 0, 0, 0.3)';
+
   return (
-    <div className="login-container">
+    <div className="login-container" style={{
+      backgroundImage: `linear-gradient(${overlayColor}, ${overlayColor}), url(${loginbg.src})`,
+      backgroundRepeat: "no-repeat",
+      backgroundSize: "cover",
+      backgroundAttachment: "fixed",
+
+    }}>
+
       <form onSubmit={handleLogin} className="login-form">
-        <h1 style={{ fontSize: '2em', marginBottom: '30px', textAlign: 'center' }}>Login Test</h1>
+        <h1 style={{ fontSize: '2em', marginBottom: '30px', marginTop:'30px',textAlign: 'center', fontFamily: 'zzzTH' }}>Log in to CapstoneHub</h1>
         
-        <div style={{ marginBottom: '0px' }}>
+        <div className='login-info-container'>
+          <input
+            type="text"
+            placeholder="Username"
+            name="username"
+            // value={Info.firstname}
+            // onChange={HandleInfo}
+          className='login-info'
+          />
+
+          <input
+            type="text"
+            placeholder="Password"
+            name="password"
+            // value={Info.firstname}
+            // onChange={HandleInfo}
+          className='login-info'
+          />
+        </div>
+        <div className='login-button-container'>
+          <div style={{ marginBottom: '0px' }}>
           <button
-            type="submit" 
+            type="submit"
             className="login-button"
           >
-            กดเพื่อเข้า Dashboard
+            Login
           </button>
         </div>
+        </div>
       </form>
+
     </div>
   );
 }
