9c334f3b2cc918495270e1bf894c35cc201e5ca6
Unsull <104518389+Unsull@users.noreply.github.com>
2025-12-04 17:01:19 +0700

    validate Group Project - close #41

--- backend/entity/GroupProject.go
+++ backend/entity/GroupProject.go
@@ -8,16 +8,16 @@ type GroupProject struct {
 	gorm.Model
 	GroupNumber uint   `json:"group_number" valid:"required,range(1|40)~GroupNumber must be between 1 and 40"`
 	GroupStatus string `json:"group_status" valid:"required~GroupStatus is required"`
-	Membership	int		`json:"membership" valid:"required,range(3|5)~Membership must be between 3 and 5"`	// จำนวนสมาชิกต่อกลุ่ม
-	
-	// Teacher ต้องว่าง หากยังไม้เลือกอาจารย์
-	TeacherID   *uint  `json:"teacher_id"`
-	Teacher     *User `gorm:"foreignKey:TeacherID" json:"teacher"`
+	Membership  int    `json:"membership" valid:"required,range(3|5)~Membership must be between 3 and 5"` // จำนวนสมาชิกต่อกลุ่ม
 
-	GroupMembers []GroupMember `gorm:"foreignKey:GroupProjectID" json:"group_members"`
-	SelectAdvisors []SelectAdvisor `gorm:"foreignKey:GroupProjectID" json:"select_advisors"`
-	Progress   []Progress    `gorm:"foreignKey:GroupProjectID" json:"progress"`
-	Appointment  []Appointment `gorm:"foreignKey:GroupProjectID" json:"appointments"`
+	// Teacher ต้องว่าง หากยังไม้เลือกอาจารย์ที่ปรึกษา
+	TeacherID *uint `json:"teacher_id"`
+	Teacher   *User `gorm:"foreignKey:TeacherID" json:"teacher"`
+
+	GroupMembers    []GroupMember    `gorm:"foreignKey:GroupProjectID" json:"group_members"`
+	SelectAdvisors  []SelectAdvisor  `gorm:"foreignKey:GroupProjectID" json:"select_advisors"`
+	Progress        []Progress       `gorm:"foreignKey:GroupProjectID" json:"progress"`
+	Appointment     []Appointment    `gorm:"foreignKey:GroupProjectID" json:"appointments"`
 	TopicSelections []TopicSelection `gorm:"foreignKey:GroupProjectID" json:"topic_selections"`
-	Topic 	 []Topic        `gorm:"foreignKey:GroupProjectID" json:"topics"`
+	Topic           []Topic          `gorm:"foreignKey:GroupProjectID" json:"topics"`
 }
