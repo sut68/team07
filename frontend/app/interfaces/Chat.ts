cc5467011ba0a0717d555f9241f9297dcf31ec5e
Yukita1226 <thammarat.songwiwat@gmail.com>
2025-12-06 14:13:20 +0700

    add chat interface prepare of fire api -close  #14

--- frontend/app/interfaces/Chat.ts
+++ frontend/app/interfaces/Chat.ts
@@ -1,6 +1,27 @@
-export interface Chat {
-    id: number;
-    process_id: number,
-    sender_id: number;
-    message: string;
+export interface FullChat {
+  id: number;
+  group_member_id: number;
+  process_id: number;
+  sender_id: number;
+  messege: string;
+  created_at: Date;
+  updated_at: Date;
+}
+
+export interface ChatCreate {
+  group_member_id: number;
+  process_id: number;
+  sender_id: number;
+  messege: string;
+}
+
+export interface getChat {
+  process_id: number;
+  group_member_id: number;
+}
+
+export interface ChatDelete {
+  id: number;
+  group_member_id: number;
+  process_id: number;
 }
