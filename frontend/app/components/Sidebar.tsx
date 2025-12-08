325652f1dc243980c68ceea73a5bf02170c15aff
Yukita1226 <thammarat.songwiwat@gmail.com>
2025-12-08 13:17:54 +0700

    fix small bug

--- frontend/app/components/Sidebar.tsx
+++ frontend/app/components/Sidebar.tsx
@@ -4,12 +4,13 @@ const roleNavItems: { [key: string]: { name: string; href: string }[] } = {
   Admin: [
     { name: 'Dashboard Admin', href: '/admin/dashboard' },
     { name: 'จัดการผู้ใช้', href: '/admin/users' },
+    { name: 'Test chat', href: '/chat' }, 
   ],
   Teacher: [
     { name: 'Dashboard Teacher', href: '/teacher/dashboard' },
     { name: 'กำหนดการสอน', href: '/teacher/schedule' },
 
-    { name: 'Test chat', href: '/chat/chat' }, 
+    { name: 'Test chat', href: '/chat' },  
   ],
   Student: [
     { name: 'Dashboard Student', href: '/student/dashboard' },
