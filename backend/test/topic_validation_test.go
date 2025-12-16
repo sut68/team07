fa1762198259de3667d886614409ca569658938c
Rattasat160947 <rattasat0624987765@gmail.com>
2025-12-16 16:35:45 +0700

    Delete Validate Topic

--- backend/test/topic_validation_test.go
+++ backend/test/topic_validation_test.go
@@ -18,7 +18,6 @@ func TestTopic(t *testing.T) {
 			Scope:          "Scope",
 			Description:    "Description about the topic",
 			Status:         "Pending",
-			Proposer_role:  "Student",
 		}
 
 		ok, err := govalidator.ValidateStruct(topic)
@@ -34,7 +33,6 @@ func TestTopic(t *testing.T) {
 			Scope:          "Scope",
 			Description:    "Description about the topic",
 			Status:         "Pending",
-			Proposer_role:  "Student",
 		}
 
 		ok, err := govalidator.ValidateStruct(topic)
@@ -50,7 +48,6 @@ func TestTopic(t *testing.T) {
 			Scope:          "", // ผิดตรงนี้
 			Description:    "Description about the topic",
 			Status:         "Pending",
-			Proposer_role:  "Student",
 		}
 
 		ok, err := govalidator.ValidateStruct(topic)
@@ -66,7 +63,6 @@ func TestTopic(t *testing.T) {
 			Scope:          "Scope",
 			Description:    "", // ผิดตรงนี้
 			Status:         "Pending",
-			Proposer_role:  "Student",
 		}
 
 		ok, err := govalidator.ValidateStruct(topic)
@@ -82,7 +78,6 @@ func TestTopic(t *testing.T) {
 			Scope:          "Scope",
 			Description:    "Description about the topic",
 			Status:         "", // ผิดตรงนี้
-			Proposer_role:  "Student",
 		}
 
 		ok, err := govalidator.ValidateStruct(topic)
@@ -91,20 +86,5 @@ func TestTopic(t *testing.T) {
 		g.Expect(err.Error()).To(Equal("Status is required"))
 	})
 
-	t.Run(`ProposerRole is required`, func(t *testing.T) {
-		topic := &entity.Topic{
-			Title:          "System Analysis Topic",
-			Objective:      "Objective",
-			Scope:          "Scope",
-			Description:    "Description about the topic",
-			Status:         "Pending",
-			Proposer_role:  "", // ผิดตรงนี้
-		}
-
-		ok, err := govalidator.ValidateStruct(topic)
-		g.Expect(ok).NotTo(BeTrue())
-		g.Expect(err).NotTo(BeNil())
-		g.Expect(err.Error()).To(Equal("ProposerRole is required"))
-	})
 
 }
