17ca83511a34bc5635d86bbc1b054be4d56eb4d7
Rattasat160947 <rattasat0624987765@gmail.com>
2025-12-25 22:35:03 +0700

    Add Test

--- backend/entity/News.go
+++ backend/entity/News.go
@@ -4,10 +4,10 @@ import "gorm.io/gorm"
 
 type News struct {
 	gorm.Model
-	Title       string `gorm:"type:varchar(255);not null"`
-	Description string `gorm:"type:text;not null"`
+	Title       string `gorm:"type:varchar(255);not null" valid:"required~Title is required"`
+	Description string `gorm:"type:text;not null" valid:"required~Description is required"`
 	File        string `gorm:"type:varchar(512)"`
-	Category    string `gorm:"type:varchar(50);not null"`
+	Category    string `gorm:"type:varchar(50);not null" valid:"required~Category is required"`
 
 	TeacherID uint  `json:"teacher_id"`
 	Teacher   *User `gorm:"foreignKey:TeacherID" json:"teacher"`
