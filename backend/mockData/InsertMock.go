43062101d4451f638f20671c257efaf99789bfcb
Ubuntu <azureuser@se.0rtj4qu3oq4u5ojxixirxd1hue.jktx.internal.cloudapp.net>
2026-01-15 10:50:40 +0000

    Edit after push

--- backend/mockData/InsertMock.go
+++ backend/mockData/InsertMock.go
@@ -26,7 +26,12 @@ func InsertMock(db *gorm.DB) {
 	seed(tx, &MockIssueType)
 	seed(tx, &MockActionType)
 	seed(tx, &MockUser)
+	// seed(tx, &MockGroupProject)
+	// seed(tx, &MockGroupMember)
+
 	seed(tx, &MockRoom)
+	// seed(tx, &MockTopics)
+
 	seed(tx, &MockAppointmentType)
 	seed(tx, &MockEvaluation)
 	seed(tx, &MockCriteria)
